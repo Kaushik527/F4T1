fa3f32fd227f1fb76d2b7913f6056ae124e7f774
Kaushik527 <117893701+Kaushik527@users.noreply.github.com>
2023-08-12 22:30:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import States1 from "./Components/States1";
+
+const App = () => {
+    return(
+        <div>
+            <States1/>
+        </div>
+    )
+}
+
+
+
+export default App;
